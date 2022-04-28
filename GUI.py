d8a44de58d0109c0bd35255043381ab989f90262
Vanshika-2112 <86337713+Vanshika-2112@users.noreply.github.com>
2022-04-28 18:16:31 +0530

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,78 @@
+import tkinter as tk
+root = tk.Tk()
+root.geometry("950x600")
+root.title("Python Interpreter")
+canvas1 = tk.Canvas(root, width=1000, bg="black", height=1000)
+canvas1.pack()
+
+label1 = tk.Label(
+    root, text="Welcome to my Python :)", fg="white", bg="black", font=("SansSerif", 24, "bold")
+)
+label1.place(x=280, y=50)
+
+label2 = tk.Label(
+    root, text="Enter Your Code:", fg="white", bg="black", font=("SansSerif", 14)
+)
+label2.place(x=50, y=130)
+
+label3 = tk.Label(
+    root, text="User Variables", fg="white", bg="black", font=("SansSerif", 14)
+)
+label3.place(x=500, y=350)
+
+label4 = tk.Label(
+    root, text="Output", fg="white", bg="black", font=("SansSerif", 14)
+)
+label4.place(x=55, y=350)
+
+canvas2 = tk.Canvas(root, width = 400, height = 300)
+canvas2.pack()
+my_text = tk.Text(root)
+my_text.pack(pady=20)
+canvas1.create_window(250, 240, width=400, height=100, window=my_text)
+
+canvas3 = tk.Canvas(root, width = 400, height = 300)
+canvas3.pack()
+my_text1 = tk.Text(root)
+my_text1.pack(pady=20)
+canvas1.create_window(700, 240, width=400, height=100, window=my_text1)
+
+canvas4 = tk.Canvas(root, width = 400, height = 300)
+canvas4.pack()
+my_text2 = tk.Text(root)
+my_text2.pack(pady=20)
+canvas1.create_window(700, 450, width=400, height=100, window=my_text2)
+
+canvas5 = tk.Canvas(root, width = 400, height = 300)
+canvas5.pack()
+my_text3 = tk.Text(root)
+my_text3.pack(pady=20)
+canvas1.create_window(250, 450, width=400, height=100, window=my_text3)
+
+def out():
+    code = my_text.get(1.0, tk.END)
+    text_file = open('mypython_input.txt', 'w+')
+    text_file.write(code)
+
+button1 = tk.Button(
+    text="Generate Output", bg="#4EEE94", font=("SansSerif", 12), command = out
+    )
+canvas1.create_window(240, 320, width=140, height=30, window=button1)
+
+button2 = tk.Button(
+    text="Clear", bg="#4EEE94", font=("SansSerif", 12), command = out
+    )
+canvas1.create_window(560, 160, width=80, height=30, window=button2)
+
+button3 = tk.Button(
+    text="Save", bg="#4EEE94", font=("SansSerif", 12), command = out
+    )
+canvas1.create_window(660, 160, width=80, height=30, window=button3)
+
+button4 = tk.Button(
+    text="Get User Variables", bg="#4EEE94", font=("SansSerif", 12), command = out
+    )
+canvas1.create_window(760, 360, width=180, height=30, window=button4)
+
+
+root.mainloop()
